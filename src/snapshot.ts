3773ebd8e395a7ab68a63fb5570a093c28b92ca3
Wictor Wilén <wictor@wictorwilen.se>
2024-01-02 17:02:38 +0100

    Update Node.js versions and package dependencies

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Wictor Wilén. All rights reserved. 
 // Licensed under the MIT license.
 
-import { writeFile, mkdirSync, existsSync } from 'fs';
+import { writeFileSync, mkdirSync, existsSync } from 'fs';
 import { RingApi } from 'ring-client-api'
 import * as path from 'path'
 import * as dotenv from "dotenv";
@@ -9,7 +9,8 @@ import * as lodash from "lodash";
 
 const log = console.log;
 
-async function snapshot() {
+
+const snapshot = async (): Promise<void> => {
     log("running snapshot")
     const ringApi = new RingApi({
         refreshToken: process.env.TOKEN as string,
@@ -23,30 +24,35 @@ async function snapshot() {
         mkdirSync(path.resolve(__dirname, "target"));
     }
 
-    cameras.forEach(camera => {
+    for (const camera of cameras) {
+        // cameras.forEach(async camera => {
         const name = lodash.camelCase(camera.name);
         log(`Retrieving snapshot for ${camera.name}`);
+        try {
+            const result = await camera.getSnapshot();
 
-        camera.getSnapshot().then(function (result) {
-            try {
-                log((path.resolve(__dirname, "target", name)));
-                if (!existsSync(path.resolve(__dirname, "target", name))) {
-                    mkdirSync(path.resolve(__dirname, "target", name));
-                }
-                writeFile(path.resolve(__dirname, "target", path.join(name, Date.now() + '.png')), result, (err) => {
-                    if (err) throw err;
-                    log('Saved!');
-                });
-            } catch (err) {
-                log(`Error: ${err}`);
+            log((path.resolve(__dirname, "target", name)));
+            if (!existsSync(path.resolve(__dirname, "target", name))) {
+                mkdirSync(path.resolve(__dirname, "target", name));
             }
+            writeFileSync(path.resolve(__dirname, "target", path.join(name, Date.now() + '.png')), result, );
+            log(`Snapshot for ${camera.name} saved`);
 
-        }).catch(err => {
+        }
+        catch (err) {
             log(`Snapshot error: ${err}`);
-        })
-    });
+        }
+    
+
+    };
 }
 
 dotenv.config();
 
-snapshot()
+snapshot() .then(() => {
+    log("done");
+    process.exit(0);
+})
+.catch(err => {
+    log(err)
+});
